17d96c680499631aee7accccbac8b3f4bb5afb7a
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-12-08 17:08:13 -0300

    refactor: Alterando para ExpressD

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const http = require("http")
-
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-})
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, 'public')));
